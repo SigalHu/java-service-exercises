3ea683bf14685867d25ab3b8d7a8befa7321c3fe
huxujun <huxujun@didichuxing.com>
2018-08-17 13:12:09 +0800

    编写基本的控制器

---
+++ jse-springmvc/src/main/java/com/sigalhu/jse/springmvc/spittr/web/HomeController.java
@@ -0,0 +1,23 @@
+package com.sigalhu.jse.springmvc.spittr.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author huxujun
+ * @date 2018/8/17
+ */
+//声明为一个控制器
+@Controller
+//该注解声明来所要处理的请求
+//value属性指定了该方法所要处理的请求路径
+@RequestMapping({"/", "/homepage"})
+public class HomeController {
+
+    //method属性细化了它所处理的Http方法
+    @RequestMapping(method = RequestMethod.GET)
+    public String home() {
+        return "home";
+    }
+}
---
+++ jse-springmvc/src/test/java/com/sigalhu/jse/springmvc/spittr/web/HomeControllerTest.java
@@ -0,0 +1,22 @@
+package com.sigalhu.jse.springmvc.spittr.web;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+/**
+ * @author huxujun
+ * @date 2018/8/17
+ */
+public class HomeControllerTest {
+
+    @Test
+    public void home() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.view().name("home"));
+    }
+}
