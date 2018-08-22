34b94a524316a119fd526cc48d6fae59a5203522
huxujun <huxujun@didichuxing.com>
2018-08-22 23:30:49 +0800

    Cleanup注解

---
+++ jse-lombok/src/test/java/com/sigalhu/jse/lombok/cleanup/Example1Test.java
@@ -0,0 +1,17 @@
+package com.sigalhu.jse.lombok.cleanup;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author huxujun
+ * @date 2018/8/22
+ */
+public class Example1Test {
+
+    @Test
+    public void example() {
+        Example1.example();
+    }
+}
