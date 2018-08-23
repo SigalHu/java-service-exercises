acaab0e72dbd5c45e1f42b9c41a64ca867cebf56
huxujun <huxujun@didichuxing.com>
2018-08-24 01:28:08 +0800

    update @ToString

--- jse-lombok/src/main/java/com/sigalhu/jse/lombok/tostring/Example1.java
+++ jse-lombok/src/main/java/com/sigalhu/jse/lombok/tostring/Example1.java
@@ -9,12 +9,19 @@ import lombok.ToString;
 //callSuper属性为true时调用父类toString函数
 @ToString(callSuper = true)
 public class Example1 extends Square {
+
     //ToString注解默认只打印非静态成员变量，可通过Include注解打印
     //rank属性的值越高，优先级越高，默认为0
-    @ToString.Include(rank = 1) private static final int STATIC_VAR = 10;
+    @ToString.Include(rank = 1)
+    private static final int STATIC_VAR = 10;
+
     //通过Exclude注解排除不想打印的成员变量
-    @ToString.Exclude private int id;
-    @ToString.Include(rank = 2) private String name;
+    @ToString.Exclude
+    private int id;
+
+    @ToString.Include(rank = 2)
+    private String name;
+
     private String[] tags;
 
     public Example1(int id, String name, int width, int height, String[] tags) {
@@ -29,8 +36,11 @@ public class Example1 extends Square {
 //includeFieldNames属性为false时，不打印字段名
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
 class Square {
+
     //通过name属性可以改变打印的字段名
-    @ToString.Include(name = "other name") private final int width;
+    @ToString.Include(name = "other name")
+    private final int width;
+
     private final int height;
 
     public Square(int width, int height) {
