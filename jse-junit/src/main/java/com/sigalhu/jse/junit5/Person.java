91a47f9b1efb2eb40d9a3e4d5721e77fcd91e815
huxujun <huxujun@didichuxing.com>
2018-09-17 01:58:09 +0800

    junit5断言的使用

---
+++ jse-junit/src/main/java/com/sigalhu/jse/junit5/Person.java
@@ -0,0 +1,15 @@
+package com.sigalhu.jse.junit5;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author huxujun
+ * @date 2018/9/17
+ */
+@Data
+@AllArgsConstructor
+public class Person {
+    private String firstName;
+    private String lastName;
+}
