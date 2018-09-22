52ac8ca8b628bbe14acd236ee6e590f056e68d28
huxujun <huxujun@didichuxing.com>
2018-09-22 12:59:20 +0800

    更新注释

--- jse-mockito/src/test/java/com/sigalhu/jse/mockito/VerifyTest.java
+++ jse-mockito/src/test/java/com/sigalhu/jse/mockito/VerifyTest.java
@@ -6,6 +6,9 @@ import org.mockito.Mockito;
 import java.util.List;
 
 /**
+ * 一旦一个mock对象被创建，那么该对象所有交互行为都会被记住。
+ * 比如下面例子中它可以记住mockedList某个方法是否被调用过或者被调用过几次。
+ *
  * @author huxujun
  * @date 2018/9/22
  */
