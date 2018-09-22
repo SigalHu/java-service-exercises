f796108130410312e595750e129b01dd4cd937dd
huxujun <huxujun@didichuxing.com>
2018-09-22 20:46:24 +0800

    验证调用的具体次数/最少次数/从未调用

--- jse-mockito/src/test/java/com/sigalhu/jse/mockito/VerifyTest.java
+++ jse-mockito/src/test/java/com/sigalhu/jse/mockito/VerifyTest.java
@@ -25,11 +25,37 @@ public class VerifyTest {
         mockedList.clear();
 
         //verification
-        //验证add方法是否在前面被调用了一次，且参数为“one”。clear方法同样
+        //验证add方法是否在前面被调用了一次，且参数为“one”，clear方法同样
         Mockito.verify(mockedList).add("one");
         Mockito.verify(mockedList).clear();
 
-        //下面的验证会失败。因为没有调用过add("two")
+        //下面的验证会失败，因为没有调用过add("two")
         Mockito.verify(mockedList).add("two");
+
+        //using mock
+        mockedList.add("once");
+
+        mockedList.add("twice");
+        mockedList.add("twice");
+
+        mockedList.add("three times");
+        mockedList.add("three times");
+        mockedList.add("three times");
+
+        //下面两种写法都是针对调用1次，因为1是默认的，我们一般使用第一种写法就可以
+        Mockito.verify(mockedList).add("once");
+        Mockito.verify(mockedList, Mockito.times(1)).add("once");
+
+        //指定了具体调用的次数
+        Mockito.verify(mockedList, Mockito.times(2)).add("twice");
+        Mockito.verify(mockedList, Mockito.times(3)).add("three times");
+
+        //使用了never()，即times(0)
+        Mockito.verify(mockedList, Mockito.never()).add("never happened");
+
+        //使用了最多多少次，最少多少次
+        Mockito.verify(mockedList, Mockito.atLeastOnce()).add("three times");
+        Mockito.verify(mockedList, Mockito.atLeast(2)).add("three times");
+        Mockito.verify(mockedList, Mockito.atMost(5)).add("three times");
     }
 }
