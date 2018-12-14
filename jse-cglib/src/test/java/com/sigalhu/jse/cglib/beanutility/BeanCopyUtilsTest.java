a9b954c56c1230b116b26cec8c815d9e1424205f
huxujun <huxujun@didichuxing.com>
2018-12-14 17:05:02 +0800

    测试cglib的BeanCopier

--- jse-cglib/src/test/java/com/sigalhu/jse/cglib/beanutility/BeanCopyUtilsTest.java
+++ jse-cglib/src/test/java/com/sigalhu/jse/cglib/beanutility/BeanCopyUtilsTest.java
@@ -5,6 +5,13 @@ import com.sigalhu.jse.cglib.targetobject.DogImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
 /**
  * @author huxujun
  * @date 2018/11/18
@@ -15,21 +22,65 @@ public class BeanCopyUtilsTest {
     public void copy() throws Exception {
         DogImpl dog = new DogImpl("BeanCopyUtilsTest");
         Assert.assertEquals("BeanCopyUtilsTest", dog.getName());
-        CatImpl cat = BeanCopyUtils.copy(dog, CatImpl.class);
+        CatImpl cat = new CatImpl();
+        BeanCopyUtils.copy(dog, cat);
         Assert.assertEquals("BeanCopyUtilsTest", cat.getName());
     }
 
     @Test
     public void test() throws Exception {
-        int count = 10000;
+        int count = 10000000;
         DogImpl dog = new DogImpl("BeanCopyUtilsTest");
 
         long time = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
-            CatImpl cat = BeanCopyUtils.copy(dog, CatImpl.class);
+            CatImpl cat = new CatImpl();
+            BeanCopyUtils.copy(dog, cat);
         }
         time = System.currentTimeMillis() - time;
         System.out.println("Using BeanCopier: " + time + "ms");
+
+        time = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            CatImpl cat = new CatImpl();
+            BeanCopyUtils.staticCopy(dog, cat);
+        }
+        time = System.currentTimeMillis() - time;
+        System.out.println("Using static BeanCopier: " + time + "ms");
+
+        Field dogName = DogImpl.class.getDeclaredField("name");
+        dogName.setAccessible(true);
+        Field catName = CatImpl.class.getDeclaredField("name");
+        catName.setAccessible(true);
+        time = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            CatImpl cat = new CatImpl();
+            catName.set(cat, dogName.get(dog));
+        }
+        time = System.currentTimeMillis() - time;
+        System.out.println("Using Field: " + time + "ms");
+
+        BeanInfo dogInfo = Introspector.getBeanInfo(DogImpl.class);
+        Method dogNameRead = null;
+        for (PropertyDescriptor propertyDescriptor : dogInfo.getPropertyDescriptors()) {
+            if ("name".equals(propertyDescriptor.getName())) {
+                dogNameRead = propertyDescriptor.getReadMethod();
+            }
+        }
+        BeanInfo catInfo = Introspector.getBeanInfo(CatImpl.class);
+        Method catNameWrite = null;
+        for (PropertyDescriptor propertyDescriptor : catInfo.getPropertyDescriptors()) {
+            if ("name".equals(propertyDescriptor.getName())) {
+                catNameWrite = propertyDescriptor.getWriteMethod();
+            }
+        }
+        time = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            CatImpl cat = new CatImpl();
+            catNameWrite.invoke(cat, dogNameRead.invoke(dog));
+        }
+        time = System.currentTimeMillis() - time;
+        System.out.println("Using Method: " + time + "ms");
 
         time = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
