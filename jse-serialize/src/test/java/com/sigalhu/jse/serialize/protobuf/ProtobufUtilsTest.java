bc33c096f4e8381f3b09cdedb43900971d662d2f
huxujun <sigalhu@foxmail.com>
2019-09-29 14:20:56 +0800

    测试 proto2 和 3 互转

--- jse-serialize/src/test/java/com/sigalhu/jse/serialize/protobuf/ProtobufUtilsTest.java
+++ jse-serialize/src/test/java/com/sigalhu/jse/serialize/protobuf/ProtobufUtilsTest.java
@@ -3,13 +3,9 @@ package com.sigalhu.jse.serialize.protobuf;
 import com.sigalhu.jse.serialize.SerializeTest;
 import com.sigalhu.jse.serialize.beans.Person;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Date;
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 /**
  * @author huxujun
@@ -18,7 +14,7 @@ import static org.junit.Assert.*;
 public class ProtobufUtilsTest extends SerializeTest {
 
     @Test
-    public void test() throws Exception {
+    public void testProto2() throws Exception {
         byte[] bytes = ProtobufUtils.toBytes(person);
         Person p = ProtobufUtils.parseBytes(bytes);
         Assert.assertNotEquals(person, p);
@@ -27,9 +23,45 @@ public class ProtobufUtilsTest extends SerializeTest {
     }
 
     @Test
-    public void testList() {
+    public void testProto2List() {
         byte[] bytes = ProtobufUtils.toDelimitedBytes(people);
         List<Person> ps = ProtobufUtils.parseDelimitedBytes(bytes);
+        Assert.assertNotEquals(people, ps);
+        people.forEach(p -> p.setUpdateTime(null));
+        Assert.assertEquals(people, ps);
+    }
+
+    @Test
+    public void testProto3() throws Exception {
+        byte[] bytes = ProtobufUtils.toBytes3(person);
+        Person p = ProtobufUtils.parseBytes3(bytes);
+        Assert.assertNotEquals(person, p);
+        person.setUpdateTime(null);
+        Assert.assertEquals(person, p);
+    }
+
+    @Test
+    public void testProto3List() {
+        byte[] bytes = ProtobufUtils.toDelimitedBytes3(people);
+        List<Person> ps = ProtobufUtils.parseDelimitedBytes3(bytes);
+        Assert.assertNotEquals(people, ps);
+        people.forEach(p -> p.setUpdateTime(null));
+        Assert.assertEquals(people, ps);
+    }
+
+    @Test
+    public void testProto2To3() throws Exception {
+        byte[] bytes = ProtobufUtils.toBytes(person);
+        Person p = ProtobufUtils.parseBytes3(bytes);
+        Assert.assertNotEquals(person, p);
+        person.setUpdateTime(null);
+        Assert.assertEquals(person, p);
+    }
+
+    @Test
+    public void testProto2To3List() {
+        byte[] bytes = ProtobufUtils.toDelimitedBytes(people);
+        List<Person> ps = ProtobufUtils.parseDelimitedBytes3(bytes);
         Assert.assertNotEquals(people, ps);
         people.forEach(p -> p.setUpdateTime(null));
         Assert.assertEquals(people, ps);
--- jse-serialize/src/test/java/com/sigalhu/jse/serialize/protostuff/ProtostuffUtilsTest.java
+++ jse-serialize/src/test/java/com/sigalhu/jse/serialize/protostuff/ProtostuffUtilsTest.java
@@ -8,8 +8,6 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 /**
  * @author huxujun
  * @date 2019/9/28
