50d54bb02aa24c32f27538d85cbff3c5ef162f81
huxujun <sigalhu@foxmail.com>
2019-11-10 20:17:27 +0800

    通过内嵌 POJO 实时统计词频

--- jse-flink/src/main/java/com/sigalhu/jse/flink/streaming/WordStatStreamingJobWithNestedPojo.java
+++ jse-flink/src/main/java/com/sigalhu/jse/flink/streaming/WordStatStreamingJobWithNestedPojo.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -14,12 +13,12 @@ import org.apache.flink.util.Collector;
 import org.apache.flink.util.StringUtils;
 
 /**
- * 通过 POJO 实时统计词频，运行之前控制台执行 nc -lk 9999
+ * 通过内嵌 POJO 实时统计词频，运行之前控制台执行 nc -lk 9999
  *
  * @author huxujun
  * @date 2019/11/8
  */
-public class WordStatStreamingJobWithPojo {
+public class WordStatStreamingJobWithNestedPojo {
 
     public static void main(String[] args) throws Exception {
 
@@ -39,11 +38,11 @@ public class WordStatStreamingJobWithPojo {
             public void flatMap(String line, Collector<WordStatInfo> collector) throws Exception {
                 for (String word : line.toLowerCase().split(" ")) {
                     if (!StringUtils.isNullOrWhitespaceOnly(word)) {
-                        collector.collect(new WordStatInfo(word, 1));
+                        collector.collect(new WordStatInfo(new ComplexNestedClass(1, 2, Tuple3.of(3L, 4L, word)), 1));
                     }
                 }
             }
-        }).keyBy("word").timeWindow(Time.seconds(5)).sum("count").print();
+        }).keyBy("complex.word.f2").timeWindow(Time.seconds(5)).sum("complex.word.f1").print();
 
         env.execute("Flink Streaming Java API Skeleton");
     }
@@ -52,7 +51,16 @@ public class WordStatStreamingJobWithPojo {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class WordStatInfo {
-        private String word;
+        private ComplexNestedClass complex;
         private int count;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ComplexNestedClass {
+        private Integer someNumber;
+        private float someFloat;
+        private Tuple3<Long, Long, String> word;
     }
 }
